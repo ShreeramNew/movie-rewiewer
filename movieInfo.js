3202b692414e5d135823f957e9aa05b2a411fb47
ShreeramNew <shreerambca1@gmail.com>
2023-04-14 13:06:22 +0530

    feat-write review feature added

--- movieInfo.js
+++ movieInfo.js
@@ -8,7 +8,17 @@ const title=document.querySelector(".title");
 const story=document.querySelector(".story");
 const ratings=document.querySelector(".ratings")
 const year=document.querySelector(".year");
-const writeReview=document.querySelector(".new-review");
+let text=document.getElementById("review-text");
+let buttonsContainer=document.createElement("div");
+buttonsContainer.className="save-cancel-container";
+
+buttonsContainer.innerHTML=
+`
+<button class="cancel">Cancel</button>
+        <button class="save">Save</button>
+`;
+let cancel;
+let reviewContainer=document.querySelector(".write-review-container")
 
 window.addEventListener("load",()=>{
     let str=Info.Release_date;
@@ -20,5 +30,23 @@ window.addEventListener("load",()=>{
     poster.src=imageURL+Info.Poster_Path;
    
 })
+text.addEventListener("input",()=>{
+    text.style.height=text.scrollHeight+"px";
+    reviewContainer.appendChild(buttonsContainer);
+    cancel=document.querySelector(".cancel");
+    cancel.addEventListener("click",()=>{
+        text.value="";
+        text.style.height="40px";
+        try {
+            reviewContainer.removeChild(buttonsContainer)
+        } catch (error) {
+            console.log(error);
+        }
+        
+    })
+    
+
+});
+
 
 console.log(Info.Title);
