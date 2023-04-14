332d356d59401bfc8d51774f599c524a3dde1d55
ShreeramNew <shreerambca1@gmail.com>
2023-04-14 09:04:54 +0530

    shifted from then() to async await

--- movieInfo.js
+++ movieInfo.js
@@ -1,21 +1,24 @@
 const jsonInfo=localStorage.getItem("movie_Info")
 const Info=JSON.parse(jsonInfo);
 const imageURL = "https://image.tmdb.org/t/p/w500";
+
+//DOC elements
 const poster=document.querySelector(".Poster")
 const title=document.querySelector(".title");
 const story=document.querySelector(".story");
 const ratings=document.querySelector(".ratings")
 const year=document.querySelector(".year");
-var a="";
-a.substring(0,4);
+const writeReview=document.querySelector(".new-review");
+
 window.addEventListener("load",()=>{
-    var str=Info.Release_date;
+    let str=Info.Release_date;
     title.innerHTML=Info.Title;
     year.innerHTML="Year: "+str.substring(0,4);
     story.innerHTML+=Info.Story;
   
     ratings.innerHTML="Ratings:"+Info.Ratings;
     poster.src=imageURL+Info.Poster_Path;
-
+   
 })
+
 console.log(Info.Title);
